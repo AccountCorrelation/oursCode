036721b313d66eef4b3d3b547c045519578b4397
AccountCorrelation <shuyingxie1994@gmail.com>
2018-03-21 22:57:01 +0800

    Update calResultMAP.py

--- RDDcompare/calResultMAP.py
+++ RDDcompare/calResultMAP.py
@@ -1,7 +1,7 @@
 # -*- coding:  UTF-8 -*-
-#科学计算包
+#
 from numpy import *
-#运算符模块
+#
 import operator
 import sys
 import re
@@ -11,7 +11,7 @@ def start():
     mapItems=0.0
     fp=open('result.txt','r')
     for line in fp.readlines():
-        lineArr = re.split(' |,|\t',line.strip())#以空格分开,每一条边记录到
+        lineArr = re.split(' |,|\t',line.strip())#
         lenth=len(lineArr) 
         if(lenth==101 and lineArr[0].lower()!=lineArr[lenth-1].lower()):
             continue;
